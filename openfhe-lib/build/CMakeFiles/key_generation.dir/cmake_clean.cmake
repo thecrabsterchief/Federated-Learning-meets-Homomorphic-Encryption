76936d47e97e48688e7ee221393b334e471c4aba
idk-wh0am1 <i.d.k.vv.h.0.a.m.1@gmail.com>
2023-06-13 17:59:43 +0700

    openfhe-lib done

---
+++ openfhe-lib/build/CMakeFiles/key_generation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/key_generation.dir/key_generation.cpp.o"
+  "CMakeFiles/key_generation.dir/key_generation.cpp.o.d"
+  "key_generation"
+  "key_generation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/key_generation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
